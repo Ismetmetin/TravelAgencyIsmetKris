fd0231050e0fe9b34a07225205e08448167133fd
ismet <iscometbg@gmail.com>
2023-03-15 17:46:51 +0200

    Hello world nov opit

---
+++ TravelAgencyKrisAndIsmet/Data/DriverBus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Data
+{
+    public class DriverBus
+    {
+        public int DriverId { get; set; }
+        public int BusId { get; set; }
+        public Driver Driver { get; set; }
+        public Bus Bus { get; set; }
+
+        public DriverBus(int driverId, int busId)
+        {
+            DriverId = driverId;
+            BusId = busId;
+        }
+
+        public DriverBus()
+        {
+
+        }
+
+
+    }
+}
---
+++ TravelAgencyKrisAndIsmet/Presentation/Display.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.Presentation
+{
+    public class Display
+    {
+        public Display()
+        {
+            Input();
+        }
+
+        public void Input()
+        {
+            int operation = -1;
+            // TODO: Да измислим дали да направим подметоди на Input за всяко меню. Така май ще е най добрия начин.
+            do
+            {
+                ShowMainMenu();
+                operation = int.Parse(Console.ReadLine());
+                switch (operation)
+                {
+                    case 1:
+                        BusMenuInput();
+                        break;
+                    case 2:
+                        ShowCityMenu();
+                        break;
+                    case 3:
+                        ShowClientMenu();
+                        break;
+                    case 4:
+                        ShowDriverMenu();
+                        break;
+                    case 5:
+                        ShowTravelMenu();
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        Console.WriteLine("Option not available!");
+                        break;
+                }
+            } while (operation != 6);
+        }
+
+        private void BusMenuInput()
+        {
+            ShowBusMenu();
+
+            int operation = -1;
+
+            operation = int.Parse(Console.ReadLine());
+            switch (operation)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    break;
+                default:
+                    Console.WriteLine("Option not available!\nReturning to main menu...");
+                    break;
+            }
+
+        }
+
+        public void ShowMainMenu()
+        {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string('-', 18) + "MENU" + new string('-', 18));
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("1. Bus menu");
+            Console.WriteLine("2. City menu");
+            Console.WriteLine("3. Client menu");
+            Console.WriteLine("4. Driver menu");
+            Console.WriteLine("5. Travel menu");
+        }
+
+        public void ShowBusMenu()
+        {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string('-', 18) + "BUS MENU" + new string('-', 18));
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("1. Add new bus");
+            Console.WriteLine("2. Delete a bus");
+            Console.WriteLine("3. Get bus");
+            Console.WriteLine("4. Get all buses");
+            Console.WriteLine("5. Update a bus");
+        }
+
+        public void ShowCityMenu()
+        {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string('-', 18) + "BUS MENU" + new string('-', 18));
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("1. Add new city");
+            Console.WriteLine("2. Delete a city");
+            Console.WriteLine("3. Get city");
+            Console.WriteLine("4. Get all cities");
+            Console.WriteLine("5. Update a city");
+        }
+
+        public void ShowClientMenu()
+        {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string('-', 18) + "BUS MENU" + new string('-', 18));
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("1. Add new client");
+            Console.WriteLine("2. Delete a client");
+            Console.WriteLine("3. Get client");
+            Console.WriteLine("4. Get all clients");
+            Console.WriteLine("5. Update a client");
+            Console.WriteLine("5. Get client's travel");
+        }
+
+        public void ShowDriverMenu()
+        {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string('-', 18) + "BUS MENU" + new string('-', 18));
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("1. Add new driver");
+            Console.WriteLine("2. Delete a driver");
+            Console.WriteLine("3. Get driver");
+            Console.WriteLine("4. Get all drivers");
+            Console.WriteLine("5. Update a driver");
+        }
+
+        public void ShowTravelMenu()
+        {
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine(new string('-', 18) + "BUS MENU" + new string('-', 18));
+            Console.WriteLine(new string('-', 40));
+            Console.WriteLine("1. Add new travel");
+            Console.WriteLine("2. Delete a travel");
+            Console.WriteLine("3. Get travel");
+            Console.WriteLine("4. Get all travels");
+            Console.WriteLine("5. Update a travel");
+            Console.WriteLine("6. Get bus by travel id");
+            Console.WriteLine("7. Get from-city");
+            Console.WriteLine("8. Get to-city");
+        }
+    }
+}
