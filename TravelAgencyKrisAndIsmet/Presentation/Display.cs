f9748adcf00b2a5b67bb62ef56c8a2b9d67fad9a
Kris040805 <113238706+Kris040805@users.noreply.github.com>
2023-03-18 13:39:55 +0200

    Client and city displays ready

--- TravelAgencyKrisAndIsmet/Presentation/Display.cs
+++ TravelAgencyKrisAndIsmet/Presentation/Display.cs
@@ -10,8 +10,7 @@ namespace TravelAgency.Presentation
 {
     public class Display
     {
-        BusBusiness busBusiness = new BusBusiness();
-        CityBusiness cityBusiness = new CityBusiness();
+        
         ClientBusiness clientBusiness = new ClientBusiness();
         DriverBusiness driverBusiness = new DriverBusiness();
         TravelBusiness travelBusiness = new TravelBusiness();
@@ -241,263 +240,20 @@ namespace TravelAgency.Presentation
 
 
         // МОЖЕМ ДА ДОБАВИМ ИНДИКАТОР НАПРИМЕР СЛЕД КАТО СЕ ДОБАВИ АВТОБУС В БАЗАТА ДАННИ СЕ ИЗПИСВА БЪС АДДЕД РЕТЪРНИН ТО МЕЙН МЕНЙУ...
-
-        //----------------------------------------------
-        // Bus menu input and methods
-        
-       
+             
 
         //------------------------------------------------
         // Client menu, input and methods
 
 
-        /// <summary>
-        /// Принтира менюто и иска вход
-        /// </summary>
-        private void ClientMenuInput()
-        {
-
-            ShowClientMenu();
-
-            int operation = int.Parse(Console.ReadLine());
-
-            switch (operation)
-            {
-                case 1:
-                    ClientAdd();
-                    break;
-                case 2:
-                    ClientDelete();
-                    break;
-                case 3:
-                    ClientGet();
-                    break;
-                case 4:
-                    ClientGetAll();
-                    break;
-                case 5:
-                    ClientUpdate();
-                    break;
-                case 6:
-                    ClientGetTravelByClientId();
-                    break;
-                default:
-                    Console.WriteLine("Option not available!\nReturning to main menu...");
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// Manually add a new client to the database
-        /// </summary>
-        public void ClientAdd()
-        {
-            Client client = new Client();
-            Console.WriteLine("Enter first name:");
-            client.FirstName = Console.ReadLine();
-            Console.WriteLine("Enter last name:");
-            client.LastName = Console.ReadLine();
-            Console.WriteLine("Enter age :");
-            client.Age = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter id of travel");
-            client.TravelId = int.Parse(Console.ReadLine());
-            clientBusiness.Add(client);
-
-
-        }
-
-
-        /// <summary>
-        /// Given the id the method deletes a client from the database 
-        /// </summary>
-        public void ClientDelete()
-        {
-            Console.WriteLine("Enter ID to delete: ");
-            int id = int.Parse(Console.ReadLine());
-            Client client = clientBusiness.Get(id);
-            if (client != null)
-            {
-                clientBusiness.Delete(id);
-                Console.WriteLine("Done.");
-            }
-            else
-            {
-                Console.WriteLine("Client not found!");
-            }
-        }
-
-        /// <summary>
-        /// The method returns information about a client given the id
-        /// </summary>
-        public void ClientGet()
-        {
-            Console.WriteLine("Enter ID to get: ");
-            int id = int.Parse(Console.ReadLine());
-            Client client = clientBusiness.Get(id);
-            if (client != null)
-            {
-                Console.WriteLine(new string('-', 40));
-                Console.WriteLine("ID: " + client.Id);
-                Console.WriteLine("First name: " + client.FirstName);
-                Console.WriteLine("Last name: " + client.LastName);
-                Console.WriteLine("Age : " + client.Age);
-                Console.WriteLine("Travel id: " + client.TravelId);
-                Console.WriteLine(new string('-', 40));
-            }
-        }
-
-        /// <summary>
-        /// The following method returns all clients currently in the database
-        /// </summary>
-        public void ClientGetAll()
-        {
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine(new string(' ', 16) + "CLIENTS" + new string(' ', 16));
-            Console.WriteLine(new string('-', 40));
-            var clients = clientBusiness.GetAll();
-            foreach (var client in clients)
-            {
-                Console.WriteLine($"{client.Id} {client.FirstName} {client.LastName} {client.Age}, Travel ID -{client.TravelId}");
-            }
-        }
-
-        /// <summary>
-        /// актуализира информацията за клиента на когото ID-то е въведено
-        /// </summary>
-        public void ClientUpdate()
-        {
-
-            Console.WriteLine("Enter ID to update: ");
-            int id = int.Parse(Console.ReadLine());
-            Client client = clientBusiness.Get(id);
-            if (client != null)
-            {
-                Console.WriteLine("Enter first name: ");
-                client.FirstName = Console.ReadLine();
-                Console.WriteLine("Enter last name: ");
-                client.LastName = Console.ReadLine();
-                Console.WriteLine("Enter age:");
-                client.Age = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter travel id");
-                client.TravelId = int.Parse(Console.ReadLine());
-                clientBusiness.Update(client);
-            }
-            else
-            {
-                Console.WriteLine("Client not found!");
-            }
+       
 
 
 
-        }
 
-        /// <summary>
-        /// При дадено ID на клиент се връща информацията за неговото пътуване 
-        /// </summary>
-        public void ClientGetTravelByClientId()
-        {
-            Console.WriteLine("Enter the ID of a client and his travel information will be shown");
-            int idClient = int.Parse(Console.ReadLine());
-            var travel = clientBusiness.Get(idClient).Travel;
-            Console.WriteLine("Travel ID: " + travel.Id);
-            Console.WriteLine("From city:\nID: " + travel.FromCity.Id + " \nCity:" + travel.FromCity.Name);
-            Console.WriteLine("To city:\nID: " + travel.ToCity.Id + " \nCity: " + travel.ToCity.Name);
-            Console.WriteLine("Bus ID: " + travel.BusId);
-            //vij tuk
-            Console.WriteLine("Date of travel - " + travel.DateOfTravel.ToString());
-        }
 
-        // City menus
 
-        public void CityMenuInput()
-        {
-            ShowCityMenu();
-
-            int operation = int.Parse(Console.ReadLine());
-
-            switch (operation)
-            {
-                case 1: CityAdd(); break;
-                case 2: CityDelete(); break;
-                case 3: CityGet(); break;
-                case 4: CityGetAll(); break;
-                case 5: CityUpdate(); break;
-                default: Console.WriteLine("Option not available!"); break;
-            }
-        }
-
-        private void CityAdd()
-        {
-            City city = new City();
-            Console.WriteLine("Enter name of city:");
-            city.Name = Console.ReadLine();
-            Console.WriteLine("Enter population:");
-            city.Population = int.Parse(Console.ReadLine());
-            cityBusiness.Add(city);
-        }
-
-        private void CityDelete()
-        {
-            Console.WriteLine("Enter the ID of the city you want to delete:");
-            int id = int.Parse(Console.ReadLine());
-            var city = cityBusiness.Get(id);
-            if (city != null)
-            {
-                cityBusiness.Delete(id);
-                Console.WriteLine("Done.");
-            }
-            else
-            {
-                Console.WriteLine("City not found!");
-            }
-        }
-
-        private void CityGet()
-        {
-            Console.WriteLine("Enter ID to get: ");
-            int id = int.Parse(Console.ReadLine());
-            City city = cityBusiness.Get(id);
-            if (city != null)
-            {
-                Console.WriteLine(new string('-', 40));
-                Console.WriteLine("ID: " + city.Id);
-                Console.WriteLine("Name: " + city.Name);
-                Console.WriteLine("Population: " + city.Population);
-                Console.WriteLine(new string('-', 40));
-            }
-        }
-
-        private void CityGetAll()
-        {
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine(new string(' ', 16) + "CITIES" + new string(' ', 16));
-            Console.WriteLine(new string('-', 40));
-            var cities = cityBusiness.GetAll();
-            foreach (var city in cities)
-            {
-                Console.WriteLine($"{city.Id} {city.Name} Population{city.Population}");
-            }
-        }
-
-        private void CityUpdate()
-        {
-            Console.WriteLine("Enter ID to update: ");
-            int id = int.Parse(Console.ReadLine());
-            City city = cityBusiness.Get(id);
-            if (city != null)
-            {
-                Console.WriteLine("Enter name: ");
-                city.Name = Console.ReadLine();
-                Console.WriteLine("Enter population: ");
-                city.Population = int.Parse(Console.ReadLine());
-                cityBusiness.Update(city);
-            }
-            else
-            {
-                Console.WriteLine("City not found!");
-            }
-        }
-
+        // Driver menus
         public void DriverMenuInput()
         {
             ShowDriverMenu();
@@ -508,16 +264,14 @@ namespace TravelAgency.Presentation
             int operation = int.Parse(Console.ReadLine());
             switch (operation)
             {
-                case 1:DriverAdd(); break;
-                case 2:DriverDelete(); break;
-                case 3:DriverGet(); break;
-                case 4:DriverGetAll(); break;
-                case 5:DriverUpdate(); break;
+                case 1: DriverAdd(); break;
+                case 2: DriverDelete(); break;
+                case 3: DriverGet(); break;
+                case 4: DriverGetAll(); break;
+                case 5: DriverUpdate(); break;
                 default: Console.WriteLine("Option not available!\nReturning to main menu..."); break;
             }
         }
-
-        // Driver menus
 
         private void DriverAdd()
         {
@@ -618,29 +372,8 @@ namespace TravelAgency.Presentation
             Console.WriteLine("5. Travel menu");
         }
        
-        public void ShowCityMenu()
-        {
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine(new string('-', 18) + "CITY MENU" + new string('-', 18));
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine("1. Add new city");
-            Console.WriteLine("2. Delete a city");
-            Console.WriteLine("3. Get city");
-            Console.WriteLine("4. Get all cities");
-            Console.WriteLine("5. Update a city");
-        }
-        public void ShowClientMenu()
-        {
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine(new string('-', 18) + "BUS MENU" + new string('-', 18));
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine("1. Add new client");
-            Console.WriteLine("2. Delete a client");
-            Console.WriteLine("3. Get client");
-            Console.WriteLine("4. Get all clients");
-            Console.WriteLine("5. Update a client");
-            Console.WriteLine("5. Get client's travel");
-        }
+       
+       
         public void ShowDriverMenu()
         {
             Console.WriteLine(new string('-', 40));
